c233e5b1b500ea9140e3b7e518329bb5d5424f09
Noel Hostad <info@juice-e.co.uk>
2015-11-19 09:46:22 +0000

    added json liveupdate

--- gulpfile.js
+++ gulpfile.js
@@ -14,6 +14,8 @@ var jsSources = [
 	'components/scripts/template.js'
 ];
 var sassSources = ['components/sass/style.scss'];
+var htmlSources = ['builds/development/*.html'];
+var jsonSources = ['builds/development/js/*.json'];
 
 
 gulp.task('coffee', function() {
@@ -43,10 +45,22 @@ gulp.task('compass', function() {
 		.pipe(connect.reload())
 });
 
+gulp.task('html', function() {
+	gulp.src(htmlSources)
+		.pipe(connect.reload())
+});
+
+gulp.task('json', function() {
+	gulp.src('builds/development/js/*.json')
+		.pipe(connect.reload())
+});
+
 gulp.task('watch', function() {
 	gulp.watch(coffeeSources, ['coffee']);
 	gulp.watch(jsSources, ['js']);
 	gulp.watch('components/sass/*.scss', ['compass']);
+	gulp.watch(htmlSources, ['html']);
+	gulp.watch(jsonSources, ['json']);
 });
 
 gulp.task('connect', function() {
@@ -56,4 +70,4 @@ gulp.task('connect', function() {
 	});
 });
 
-gulp.task('default', ['coffee', 'js', 'compass', 'connect', 'watch']);
+gulp.task('default', ['html', 'coffee', 'json', 'js', 'compass', 'connect', 'watch']);
